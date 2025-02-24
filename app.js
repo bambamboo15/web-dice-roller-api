90d49d08220720c33a1fbe9d34c598a80a7db32f
bambamboo15 <11bambamboo@gmail.com>
2025-02-23 19:08:25 -0600

    awer

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Enabling CORS is useful so websites with different origins
+// can access our API
+app.use(cors());
 
 app.get("/", function(req, res) {
 	res.sendFile("index.html", { root: __dirname });
